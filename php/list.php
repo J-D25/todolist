4b689ba000bad953edc425288a3591151b3075cf
J-D25 <101711518+J-D25@users.noreply.github.com>
2022-10-29 20:28:10 +0200

    ADD - php/list.php : sélection de l'id de la tache

--- php/list.php
+++ php/list.php
@@ -2,7 +2,7 @@
 //Connexion à la base
 require_once("connect.php");
 
-$sql = $conn->prepare("SELECT nom, status FROM `taches_nom`");
+$sql = $conn->prepare("SELECT idtaches AS id, nom, status FROM `taches_nom`");
 $sql->execute();
 $res=$sql->fetchAll(PDO::FETCH_ASSOC);
 echo json_encode($res);

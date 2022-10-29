cd18fcfa98ae941d94c0abe4e8cc564a02ada5ad
J-D25 <101711518+J-D25@users.noreply.github.com>
2022-10-29 12:35:24 +0200

    ADD - php/connect.php : ajout de la connexion à la database

---
+++ php/connect.php
@@ -0,0 +1,17 @@
+<?php
+// Définition des variables globales
+require __DIR__ . './../vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+define('SERVER', $_ENV['SERVER']);
+define('DATABASE', $_ENV['DATABASE']);
+define('USERNAME', $_ENV['USERNAME']);
+define('PASSWORD', $_ENV['PASSWORD']);
+
+$errorCode = true;
+try {
+    $conn = new PDO("mysql:host=" . SERVER . ";dbname=" . DATABASE . "", USERNAME, PASSWORD);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    $errorCode = $e->getCode();
+}

b1863a222e398ad88c69ad29ead0589d23ead50d
J-D25 <101711518+J-D25@users.noreply.github.com>
2022-10-29 19:59:18 +0200

    ADD - js/main : ajout de l'insertion, création de fonctions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,18 @@
 const section = document.getElementById("content");
 const menu = document.getElementById("menu");
+const input = document.querySelector("input");
+const inputStatus = input.previousElementSibling;
+
+var imgAttributes = {
+    "imgSrc": ["./assets/images/empty_check.png", "./assets/images/filled_check.png"],
+    "imgAlt": ["checkbox vide", "checkbox cochée"],
+    "imgClass": [["text-gray-700"], ["text-gray-300", "line-through"]]
+}
+
+// Au chargement de la page
 window.addEventListener("load", () => {
     menu.firstElementChild.style.color = "#3a7cfd";
+    setImgAttribute(inputStatus, 0);
     // suppression des tâches existantes
     while (section.lastElementChild) {
         section.removeChild(section.lastElementChild);
@@ -10,27 +21,61 @@ window.addEventListener("load", () => {
         .then(response => response.json())
         .then(data => {
             data.forEach(tache => {
-                const div = document.createElement("div");
-                div.classList.add("bg-white", "py-4", "pl-5", "pr-2", "flex", "items-center", "border-b", "shadow-md", "lg:shadow-none");
-                const img = document.createElement("img");
-                img.classList.add("pr-3");
-                const p = document.createElement("p");
-                const imgAttributes = {
-                    "imgSrc": ["./assets/images/empty_check.png", "./assets/images/filled_check.png"],
-                    "imgAlt": ["checkbox vide", "checkbox cochée"],
-                    "imgClass": [["text-gray-700"], ["text-gray-300", "line-through"]]
-                }
-                
-                img.src = imgAttributes.imgSrc[(tache.status-1)];
-                img.alt = imgAttributes.imgAlt[(tache.status-1)];
-                p.classList.add(...imgAttributes.imgClass[(tache.status-1)]);
-                
-                const pContent = document.createTextNode(tache.nom);
-                p.appendChild(pContent);
-                div.appendChild(img);
-                div.appendChild(p);
-                section.appendChild(div);
+                showTaches(tache);
             });
 
         })
 })
+
+// Au clic sur la checkbox de l'input
+inputStatus.addEventListener("click", () => {
+    setImgAttribute(inputStatus, +!Number(inputStatus.dataset['status']));
+})
+
+// Au remplissage de l'input
+input.addEventListener("change", (event) => {
+    if (event.target.value && event.target.value != " ") {
+        let newTache = {
+            "nom": event.target.value,
+            "status": (Number(inputStatus.dataset['status']) + 1)
+        }
+        let tacheToAdd = new FormData();
+        tacheToAdd.append("nom", newTache.nom);
+        tacheToAdd.append("status", newTache.status);
+        fetch("./../php/add.php", { method: "POST", body: tacheToAdd })
+            .then(response => response.json())
+            .then(data => {
+                if (data.responseDB === true) {
+                    input.value = "";
+                    setImgAttribute(inputStatus, 0);
+                    showTaches(newTache);
+                }
+            })
+    }
+})
+
+// crée une div contenant la nouvelle tâche et l'insère
+function showTaches(tache) {
+    const div = document.createElement("div");
+    div.classList.add("bg-white", "py-4", "pl-5", "pr-2", "flex", "items-center", "border-b", "shadow-md", "lg:shadow-none");
+
+    const img = document.createElement("img");
+    img.classList.add("pr-3");
+    setImgAttribute(img, (tache.status - 1));
+
+    const p = document.createElement("p");
+    p.classList.add(...imgAttributes.imgClass[(tache.status - 1)]);
+    const pContent = document.createTextNode(tache.nom);
+    p.appendChild(pContent);
+
+    div.appendChild(img);
+    div.appendChild(p);
+    section.appendChild(div);
+}
+
+// insère la source de l'image et son status
+function setImgAttribute(img, status) {
+    img.src = imgAttributes.imgSrc[status];
+    img.alt = imgAttributes.imgAlt[status];
+    img.dataset.status = status;
+}

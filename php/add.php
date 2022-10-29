22b357e672d6f2541d90e7ead9725fc1cd5c355b
J-D25 <101711518+J-D25@users.noreply.github.com>
2022-10-29 21:00:09 +0200

    ADD - php/add.php : récupération du nombre de tâches en cours et du dernier ID inséré

--- php/add.php
+++ php/add.php
@@ -2,13 +2,22 @@
 //Connexion à la base
 require_once("connect.php");
 
-$errorCode=true;
-try{
-$sql = $conn->prepare("INSERT INTO `taches_nom` (`nom`, `status`) VALUE (:nom, :status)");
-$sql->bindParam(":nom", $_POST["nom"], PDO::PARAM_STR);
-$sql->bindParam(":status", $_POST["status"], PDO::PARAM_INT);
-$sql->execute();
+$errorCode = true;
+try {
+    $addSql = $conn->prepare("INSERT INTO `taches_nom` (`nom`, `status`) VALUE (:nom, :status)");
+    $addSql->bindParam(":nom", $_POST["nom"], PDO::PARAM_STR);
+    $addSql->bindParam(":status", $_POST["status"], PDO::PARAM_INT);
+    $addSql->execute();
 } catch (PDOException $e) {
     $errorCode = $e->getCode();
 }
-echo json_encode(["responseDB"=>$errorCode]);
+
+$lastIDSql = $conn->prepare("SELECT LAST_INSERT_ID()");
+$lastIDSql->execute();
+$ID = $lastIDSql->fetch(PDO::FETCH_COLUMN);
+
+$countSql = $conn->prepare("SELECT count(idtaches) FROM `taches_nom` WHERE status = 1");
+$countSql->execute();
+$count = $countSql->fetch(PDO::FETCH_COLUMN);
+
+echo json_encode(["responseDB" => $errorCode, "count" => $count, "id" => $ID]);

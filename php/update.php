c572e30c0d1fcca3e188f02a1d8226879781c05d
J-D25 <101711518+J-D25@users.noreply.github.com>
2022-10-29 21:00:33 +0200

    ADD - php/update.php : récupération du nombre de tâches en cours

--- php/update.php
+++ php/update.php
@@ -2,13 +2,18 @@
 //Connexion à la base
 require_once("connect.php");
 
-$errorCode=true;
-try{
-$sql = $conn->prepare("UPDATE `taches_nom` SET status = :status WHERE idtaches = :id");
-$sql->bindParam(":id", $_POST["id"], PDO::PARAM_INT);
-$sql->bindParam(":status", $_POST["status"], PDO::PARAM_INT);
-$sql->execute();
+$errorCode = true;
+try {
+    $updateSql = $conn->prepare("UPDATE `taches_nom` SET status = :status WHERE idtaches = :id");
+    $updateSql->bindParam(":id", $_POST["id"], PDO::PARAM_INT);
+    $updateSql->bindParam(":status", $_POST["status"], PDO::PARAM_INT);
+    $updateSql->execute();
 } catch (PDOException $e) {
     $errorCode = $e->getCode();
 }
-echo json_encode(["responseDB"=>$errorCode]);
+
+$countSql = $conn->prepare("SELECT count(idtaches) FROM `taches_nom` WHERE status = 1");
+$countSql->execute();
+$count = $countSql->fetch(PDO::FETCH_COLUMN);
+
+echo json_encode(["responseDB" => $errorCode, "count" => $count]);

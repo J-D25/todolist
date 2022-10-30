cebb7439bb9ef03eba027c1cbd3c273f87144e68
J-D25 <101711518+J-D25@users.noreply.github.com>
2022-10-30 21:13:30 +0100

    FIX - php/update.php : vérification de l'existence des variables avant l'update

--- php/update.php
+++ php/update.php
@@ -2,18 +2,23 @@
 //Connexion à la base
 require_once("connect.php");
 
-$errorCode = true;
-try {
-    $updateSql = $conn->prepare("UPDATE `taches_nom` SET status = :status WHERE idtaches = :id");
-    $updateSql->bindParam(":id", $_POST["id"], PDO::PARAM_INT);
-    $updateSql->bindParam(":status", $_POST["status"], PDO::PARAM_INT);
-    $updateSql->execute();
-} catch (PDOException $e) {
-    $errorCode = $e->getCode();
+if (isset($_POST["id"]) && isset($_POST["status"]) && !empty($_POST["id"]) && !empty($_POST["status"])) {
+
+    $errorCode = true;
+    try {
+        $updateSql = $conn->prepare("UPDATE `taches_nom` SET status = :status WHERE idtaches = :id");
+        $updateSql->bindParam(":id", $_POST["id"], PDO::PARAM_INT);
+        $updateSql->bindParam(":status", $_POST["status"], PDO::PARAM_INT);
+        $updateSql->execute();
+    } catch (PDOException $e) {
+        $errorCode = $e->getCode();
+    }
+
+    $countSql = $conn->prepare("SELECT count(idtaches) FROM `taches_nom` WHERE status = 1");
+    $countSql->execute();
+    $count = $countSql->fetch(PDO::FETCH_COLUMN);
+
+    echo json_encode(["responseServer" => true, "responseDB" => $errorCode, "count" => $count]);
+} else {
+    echo json_encode(["responseServer" => false]);
 }
-
-$countSql = $conn->prepare("SELECT count(idtaches) FROM `taches_nom` WHERE status = 1");
-$countSql->execute();
-$count = $countSql->fetch(PDO::FETCH_COLUMN);
-
-echo json_encode(["responseDB" => $errorCode, "count" => $count]);

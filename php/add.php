0da6246b27dc42c6182279400886dea0d395c1ca
J-D25 <101711518+J-D25@users.noreply.github.com>
2022-10-30 21:13:01 +0100

    FIX - php/add.php : vérification de l'existence des variables avant l'insertion

--- php/add.php
+++ php/add.php
@@ -2,22 +2,26 @@
 //Connexion à la base
 require_once("connect.php");
 
-$errorCode = true;
-try {
-    $addSql = $conn->prepare("INSERT INTO `taches_nom` (`nom`, `status`) VALUE (:nom, :status)");
-    $addSql->bindParam(":nom", $_POST["nom"], PDO::PARAM_STR);
-    $addSql->bindParam(":status", $_POST["status"], PDO::PARAM_INT);
-    $addSql->execute();
-} catch (PDOException $e) {
-    $errorCode = $e->getCode();
+if (isset($_POST["nom"]) && isset($_POST["status"]) && !empty($_POST["nom"]) && !empty($_POST["status"])) {
+    $errorCode = true;
+    try {
+        $addSql = $conn->prepare("INSERT INTO `taches_nom` (`nom`, `status`) VALUE (:nom, :status)");
+        $addSql->bindParam(":nom", $_POST["nom"], PDO::PARAM_STR);
+        $addSql->bindParam(":status", $_POST["status"], PDO::PARAM_INT);
+        $addSql->execute();
+    } catch (PDOException $e) {
+        $errorCode = $e->getCode();
+    }
+
+    $lastIDSql = $conn->prepare("SELECT LAST_INSERT_ID()");
+    $lastIDSql->execute();
+    $ID = $lastIDSql->fetch(PDO::FETCH_COLUMN);
+
+    $countSql = $conn->prepare("SELECT count(idtaches) FROM `taches_nom` WHERE status = 1");
+    $countSql->execute();
+    $count = $countSql->fetch(PDO::FETCH_COLUMN);
+
+    echo json_encode(["responseServer" => true, "responseDB" => $errorCode, "count" => $count, "id" => $ID]);
+} else {
+    echo json_encode(["responseServer" => false]);
 }
-
-$lastIDSql = $conn->prepare("SELECT LAST_INSERT_ID()");
-$lastIDSql->execute();
-$ID = $lastIDSql->fetch(PDO::FETCH_COLUMN);
-
-$countSql = $conn->prepare("SELECT count(idtaches) FROM `taches_nom` WHERE status = 1");
-$countSql->execute();
-$count = $countSql->fetch(PDO::FETCH_COLUMN);
-
-echo json_encode(["responseDB" => $errorCode, "count" => $count, "id" => $ID]);
